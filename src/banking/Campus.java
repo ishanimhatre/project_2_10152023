ebdb5457ea126a24ca960571d11c83e050fcd577
Ishani Mhatre <93501009+ishanimhatre@users.noreply.github.com>
2023-10-15 19:51:39 -0400

    Add files via upload

---
+++ src/banking/Campus.java
@@ -0,0 +1,32 @@
+package banking;
+
+/**
+ * Enum class for campus codes
+ * @author Ishani Mhatre
+ */
+public enum Campus {
+    NEW_BRUNSWICK(0),
+    NEWARK(1),
+    CAMDEN(2),
+    UNKNOWN(-1); // Default for invalid codes
+
+    private final int code;
+
+    Campus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static Campus fromCode(int code) {
+        for (Campus campus : values()) {
+            if (campus.code == code) {
+                return campus;
+            }
+        }
+        return UNKNOWN; //invalid codes
+    }
+}
+
