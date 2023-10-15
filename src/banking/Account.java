3cc7e02a39ef4cfb23ecfe9f4b98264b78212f66
Ishani Mhatre <93501009+ishanimhatre@users.noreply.github.com>
2023-10-15 19:47:52 -0400

    Update Account.java

--- src/banking/Account.java
+++ src/banking/Account.java
@@ -1,4 +1,56 @@
 package banking;
 
-public class Account {
+/**
+ * An abstract class that accounts for the general type of accounts
+ * @author Ishani Mhatre
+ */
+public abstract class Account implements Comparable<Account> {
+    protected Profile holder;
+    protected double balance;
+
+    public Account(Profile holder, double balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public Account() {
+
+    }
+
+    public abstract double monthlyInterest();
+
+    public abstract double monthlyFee();
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int compareTo(Account otherAccount) {
+        return this.holder.compareTo(otherAccount.holder);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Account)) {
+            return false;
+        }
+        Account otherAccount = (Account) obj;
+        return this.holder.equals(otherAccount.holder);
+    }
+
+    @Override
+    public String toString() {
+        return holder.toString();
+    }
+
 }
+
+
