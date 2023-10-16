fc3f832134c08a08bc82501f4b2f8ebe8ce3f86f
Ishani Mhatre <93501009+ishanimhatre@users.noreply.github.com>
2023-10-16 08:06:18 -0400

    Update Savings.java

--- src/banking/Savings.java
+++ src/banking/Savings.java
@@ -1,34 +1,61 @@
 package banking;
 
-/**
- * Class that defines Savings account and initializes interest rate, monthly fee, and minimum balance
- * @author Ishani Mhatre
- */
-public class Savings extends Account {
-    private static final double INTEREST_RATE = 0.04;
-    private static final double MONTHLY_FEE = 25.0;
-    private boolean isLoyal;
+public class Savings extends Account{
+    protected boolean isLoyal; //loyal customer status
+    private static final double INTEREST_RATE = 4.0;
+    protected static final int MONTHLY_FEE = 25;
 
-    public Savings(Profile holder, double balance, boolean isLoyal) {
+    public Savings(){
+
+    }
+
+    public Savings(Profile holder, double balance, boolean isLoyal){
         super(holder, balance);
         this.isLoyal = isLoyal;
     }
 
+    public boolean isLoyal() {
+        return isLoyal;
+    }
+    public void setLoyal(boolean loyal) {
+        isLoyal = loyal;
+    }
+
     @Override
     public double monthlyInterest() {
-        double interestRate = INTEREST_RATE;
-        if (isLoyal) {
-            interestRate += 0.0025; // Additional interest for loyal customers
+        if(isLoyal()){
+            double interestRate = INTEREST_RATE+0.25;
+            return (balance*(interestRate/100))/12;
         }
-        return balance * interestRate / 12;
+        else
+            return (balance*(INTEREST_RATE/100))/12;
     }
 
     @Override
     public double monthlyFee() {
-        if (balance >= 500) {
-            return 0.0;
-        } else {
+        if(balance>=500){
+            return 0;
+        }
+        else{
             return MONTHLY_FEE;
         }
+    }
+
+    @Override
+    public int compareTo(Account account) {
+        if(account instanceof Savings){
+            return super.compareTo(account);
+        }
+        else {
+            return account.getClass().getName().compareTo(this.getClass().getName());
+        }
+    }
+
+    @Override
+    public String toString(){
+        if(isLoyal)
+            return "Savings : " +holder.toString() + ": : Balance $" + balance + ": : is loyal";
+        else
+            return "Savings : " +holder.toString() + ": : Balance $" + balance;
     }
 }
