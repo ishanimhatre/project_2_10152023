e37660e5853174b941c475681d76caa634f69372
Ishani Mhatre <93501009+ishanimhatre@users.noreply.github.com>
2023-10-16 08:05:17 -0400

    Update CollegeChecking.java

--- src/banking/CollegeChecking.java
+++ src/banking/CollegeChecking.java
@@ -3,13 +3,31 @@ package banking;
 /**
  * Class for a College Checking account which extends Checking and defines interest rate and monthly fee
  */
-public class CollegeChecking extends Checking {
-    private Campus campus;
+public class CollegeChecking extends Checking{
 
-    public CollegeChecking(Profile holder, double balance, Campus campus) {
+    private Campus campus; //campus code
+    public CollegeChecking(Profile holder, double balance, Campus campus){
         super(holder, balance);
         this.campus = campus;
     }
 
+    @Override
+    public double monthlyFee() {
+        return 0.0;
+    }
+
+    @Override
+    public int compareTo(Account account) {
+        if(account instanceof Checking){
+            return super.compareTo(account);
+        }
+        else {
+            return account.getClass().getName().compareTo(this.getClass().getName());
+        }
+    }
+    @Override
+    public String toString(){
+        return "College Checking: : " +holder.toString() + ": : Balance $" + balance + ": :" + campus.name();
+    }
 
 }
